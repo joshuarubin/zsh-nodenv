2fa201c1af689e30bf3f50d0ca5b948a6c123c98
Joshua Rubin <jrubin@zvelo.com>
2014-10-29 01:22:10 -0600

    speed up nodenv load
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- nodenv.plugin.zsh
+++ nodenv.plugin.zsh
@@ -1,3 +1,20 @@
 path[1,0]="$HOME/.nodenv/bin"
 path[1,0]="$ZSH/nodenv/node-build/bin"
-eval "$(nodenv init -)"
+#eval "$(nodenv init -)"
+export NODENV_SHELL=zsh
+source "$HOME/.nodenv/completions/nodenv.zsh"
+#nodenv rehash 2>/dev/null
+nodenv() {
+  local command
+  command="$1"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "`nodenv "sh-$command" "$@"`";;
+  *)
+    command nodenv "$command" "$@";;
+  esac
+}
