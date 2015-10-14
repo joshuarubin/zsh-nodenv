f61e230c8c376984314d2a3ae168d94163cedae0
Joshua Rubin <jrubin@zvelo.com>
2015-10-14 04:20:37 -0600

    make nodenv plugin work standalone

--- nodenv.plugin.zsh
+++ nodenv.plugin.zsh
@@ -1,10 +1,14 @@
 path[1,0]="$HOME/.nodenv/bin"
-path[1,0]="$ZSH/nodenv/node-build/bin"
-#eval "$(nodenv init -)"
+path[1,0]=$0:A:h/node-build/bin
 path[1,0]="$HOME/.nodenv/shims"
+
+if [ ! -h $HOME/.nodenv ]; then
+  ln -sf $0:A:h/nodenv $HOME/.nodenv
+fi
+
 export NODENV_SHELL=zsh
 source "$HOME/.nodenv/completions/nodenv.zsh"
-#nodenv rehash 2>/dev/null
+
 nodenv() {
   local command
   command="$1"
